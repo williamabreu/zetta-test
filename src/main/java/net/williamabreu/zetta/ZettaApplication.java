5a0ec5bcfdd136a8ec53d4bbb179e42832c24e19
williamabreu <contato@williamabreu.net>
2020-08-08 14:35:50 -0300

    Renamed basic controller classes

--- src/main/java/net/williamabreu/zetta/ZettaApplication.java
+++ src/main/java/net/williamabreu/zetta/ZettaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class ZettaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServingWebContentApplication.class, args);
+        SpringApplication.run(ZettaApplication.class, args);
     }
 
 }
--- src/main/java/net/williamabreu/zetta/controllers/GreetingController.java
+++ src/main/java/net/williamabreu/zetta/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package net.williamabreu.zetta;
+package net.williamabreu.zetta.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
