e4116092888f8237ac56070c484c40e2086b783b
williamabreu <contato@williamabreu.net>
2020-08-08 15:05:01 -0300

    Add database configuration files
    Params used in development, values must be changed for another
    enviroments or production.

---
+++ src/main/java/net/williamabreu/zetta/DataConfiguration.java
@@ -0,0 +1,37 @@
+package net.williamabreu.zetta;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+@Profile("dev")
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.56.13:3306/zetta_db");
+        dataSource.setUsername("zetta");
+        dataSource.setPassword("attez");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
